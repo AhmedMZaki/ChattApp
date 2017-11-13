fd194030cfea6b41755ea1282d342a2eba0dae63
ahmed3ssem <a7med.assem@gmail.com>
2017-11-13 17:07:09 +0200

    New Design ..Lessa Sheywa

--- app/src/main/java/com/example/ahmedassem/chattapp/chatcontent.java
+++ app/src/main/java/com/example/ahmedassem/chattapp/chatcontent.java
@@ -1,92 +1,33 @@
 package com.example.ahmedassem.chattapp;
 
-import com.example.ahmedassem.chattapp.Fragment.Contact;
-import com.example.ahmedassem.chattapp.Fragment.Notification;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.annotation.SuppressLint;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.ahmedassem.chattapp.Userdata.chatuser;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import com.example.ahmedassem.chattapp.Adapter.Chatadapter;
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.example.ahmedassem.chattapp.Fragment.Contact;
 
 public class chatcontent extends AppCompatActivity {
-    private Toolbar toolbar;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-    private int[] tabIcons = {
-            R.drawable.contact,
-            R.drawable.notify
-    };
-
-
+    private FrameLayout chatframe,groupchatframe,technicalframe,notfifcationframe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatcontent);
-        /* Set Toolbar ...*/
+        chatframe=findViewById(R.id.chatframe);
+        groupchatframe=findViewById(R.id.groupchatframe);
+        technicalframe=findViewById(R.id.technicalframe);
+        notfifcationframe=findViewById(R.id.notificationframe);
+        chatframe.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceType")
+            @Override
+            public void onClick(View view) {
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            }
+        });
 
 
-        viewPager = findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
-
-        tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
-        setupTabIcons();
     }
-
-    /* Set Icons to Tabs */
-    private void setupTabIcons() {
-        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
-        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
-    }
-
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFrag(new Contact(), "Contact");
-        adapter.addFrag(new Notification(), "Notification");
-        viewPager.setAdapter(adapter);
-    }
-
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFrag(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
-    }
-
-
 
 }
