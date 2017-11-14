1ed7d3f8139960d6933d4c6d410b5f5d4ff8f78f
ahmed3ssem <a7med.assem@gmail.com>
2017-11-14 16:59:30 +0200

    New Group Chat Added

--- app/src/main/java/com/example/ahmedassem/chattapp/chatcontent.java
+++ app/src/main/java/com/example/ahmedassem/chattapp/chatcontent.java
@@ -1,13 +1,13 @@
 package com.example.ahmedassem.chattapp;
 
-import android.annotation.SuppressLint;
-import android.app.FragmentTransaction;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 
-import com.example.ahmedassem.chattapp.Fragment.Contact;
+
 
 public class chatcontent extends AppCompatActivity {
     private FrameLayout chatframe,groupchatframe,technicalframe,notfifcationframe;
@@ -19,15 +19,13 @@ public class chatcontent extends AppCompatActivity {
         groupchatframe=findViewById(R.id.groupchatframe);
         technicalframe=findViewById(R.id.technicalframe);
         notfifcationframe=findViewById(R.id.notificationframe);
-        chatframe.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceType")
+        groupchatframe.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
+            public void onClick(View v) {
+                Intent intent=new Intent(chatcontent.this,groupchat.class);
+                startActivity(intent);
             }
         });
-
-
     }
 
 }
