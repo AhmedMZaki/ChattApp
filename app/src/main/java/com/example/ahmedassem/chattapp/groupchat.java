7b4e7192df99464bf8b445b1aa27924806742814
ahmed3ssem <a7med.assem@gmail.com>
2017-11-16 13:02:08 +0200

    New Design

--- app/src/main/java/com/example/ahmedassem/chattapp/groupchat.java
+++ app/src/main/java/com/example/ahmedassem/chattapp/groupchat.java
@@ -4,6 +4,10 @@ package com.example.ahmedassem.chattapp;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -15,7 +19,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import com.example.ahmedassem.chattapp.Adapter.Groupchatadapter;
 import com.example.ahmedassem.chattapp.Userdata.GroupChat;
 import com.example.ahmedassem.chattapp.Design.MyDividerItemDecoration;
@@ -28,22 +32,24 @@ public class groupchat extends AppCompatActivity {
     private Groupchatadapter mAdapter;
     private TextView newgroupchat;
     private ImageView imageView;
-
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
+    de.hdodenhof.circleimageview.CircleImageView newchatimageview;
+    String[] listItems={"AbdelSadek Nady","Mohamed Galal", "Ibrahim Nasr", "Mohamed Serag", "Abdelaziz Hassan", "Mohamed Mamdoh", "Dallah :D "};
+    boolean[] checkedItems;
+    ArrayList<Integer> mUserItems = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.groupchat);
-        imageView=findViewById(R.id.newgroupchatimage);
-        newgroupchat=findViewById(R.id.newgroupchat);
-        recyclerView = findViewById(R.id.groupchatlist);
-        mAdapter = new Groupchatadapter(movieList,this);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.VERTICAL, 5));
-        recyclerView.setAdapter(mAdapter);
+        checkedItems = new boolean[listItems.length];
+
+        setId();
+
+        intializeRecyclerview();
 
         prepareMovieData();
+
 
 
         newgroupchat.setOnClickListener(new View.OnClickListener() {
@@ -80,11 +86,109 @@ public class groupchat extends AppCompatActivity {
     }
 
 
-    public void showChangeLangDialog() {
+    private void setId()
+    {
+        /* Set Id */
+        newchatimageview =findViewById(R.id.newgroupchatimageview);
+        imageView=findViewById(R.id.newgroupchatimage);
+        newgroupchat=findViewById(R.id.newgroupchat);
+        recyclerView = findViewById(R.id.groupchatlist);
+    }
+
+
+    private void intializeRecyclerview()
+    {
+        /* Intialize RecyclerView */
+        mAdapter = new Groupchatadapter(movieList,this);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.VERTICAL, 5));
+        recyclerView.setAdapter(mAdapter);
+
+    }
+
+
+    public void showChangeLangDialog()
+    {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         LayoutInflater inflater = this.getLayoutInflater();
         final View dialogView = inflater.inflate(R.layout.newgroupchat, null);
         dialogBuilder.setView(dialogView);
+
+        final EditText text=dialogView.findViewById(R.id.searchMultiSpinnerUnlimited);
+        Button button=dialogView.findViewById(R.id.button9);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.support.v7.app.AlertDialog.Builder mBuilder = new android.support.v7.app.AlertDialog.Builder(groupchat.this);
+                mBuilder.setTitle("Firend List");
+                mBuilder.setMultiChoiceItems(listItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
+//
+                        if(isChecked){
+                            mUserItems.add(position);
+                        }else{
+                            mUserItems.remove((Integer.valueOf(position)));
+                        }
+                    }
+                });
+
+                mBuilder.setCancelable(false);
+                mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        String item = "";
+                        for (int i = 0; i < mUserItems.size(); i++) {
+                            item = item + listItems[mUserItems.get(i)];
+                            if (i != mUserItems.size() - 1) {
+                                item = item + ", ";
+                            }
+                        }
+                        text.setText(item);
+                    }
+                });
+
+                mBuilder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                mBuilder.setNeutralButton("Clear", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        for (int i = 0; i < checkedItems.length; i++) {
+                            checkedItems[i] = false;
+                            mUserItems.clear();
+                            text.setText("");
+                        }
+                    }
+                });
+
+                android.support.v7.app.AlertDialog mDialog = mBuilder.create();
+                mDialog.show();
+            }
+        });
+
+        newchatimageview=dialogView.findViewById(R.id.newgroupchatimageview);
+
+        Button choosephoto=dialogView.findViewById(R.id.choose);
+        choosephoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create intent to Open Image applications like Gallery, Google Photos
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                // Start the Intent
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+            }
+        });
+
+
+
 
 
         dialogBuilder.setTitle("New Group Chat");
@@ -102,4 +206,36 @@ public class groupchat extends AppCompatActivity {
         AlertDialog b = dialogBuilder.create();
         b.show();
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+
+                    && null != data) {
+                // Get the Image from data
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+
+                // Set the Image in ImageView after decoding the String
+                newchatimageview.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+            } else {
+                Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
+
+
